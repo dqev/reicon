ea105ccfb60268018d6e00a9eaf98347ccde138b
Dev Chauhan <devgurjar9897@gmail.com>
2026-05-05 19:54:05 +0530

    added lenis smooth scrool

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import IconsPage from './pages/Icons';
 import IconDetail from './pages/IconDetail';
 import UsagePage from './pages/Usage';
 import PackagesPage from './pages/Packages';
+import SmoothScroll from './components/SmoothScroll';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -17,14 +18,16 @@ function ScrollToTop() {
 export default function App() {
   return (
     <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/icons" element={<IconsPage />} />
-        <Route path="/icon/:name" element={<IconDetail />} />
-        <Route path="/usage" element={<UsagePage />} />
-        <Route path="/packages" element={<PackagesPage />} />
-      </Routes>
+      <SmoothScroll>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/icons" element={<IconsPage />} />
+          <Route path="/icon/:name" element={<IconDetail />} />
+          <Route path="/usage" element={<UsagePage />} />
+          <Route path="/packages" element={<PackagesPage />} />
+        </Routes>
+      </SmoothScroll>
     </BrowserRouter>
   );
 }
